0ec1bc8f689993e62b8a91def14054b4da429ec9
Joon Kang <jooneywork@gmail.com>
2021-11-28 14:10:38 -0400

    Initial Commit

---
+++ Habit 3.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habit 3.0
+//
+//  Created by Joon Kang on 11/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Habit 3.0/Habit_3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_3_0App.swift
+//  Habit 3.0
+//
+//  Created by Joon Kang on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Habit_3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
